25cba6e01d44c69b4fac252aaa760440f587aabf
Uladzislau Ulasovich <ulasowitch.vlad@gmail.com>
2024-11-10 00:48:28 +0100

    Add Pact dependencies to build.gradle.

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.0.0"
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
+    id("au.com.dius.pact") version "4.6.15"
 }
 
 group = "com.github.uuladzislau"
@@ -10,7 +11,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -18,14 +19,23 @@ repositories {
     mavenCentral()
 }
 
+val pact_version = "4.6.15"
+val swagger_request_validator_version = "2.43.0"
+val rest_assured_version = "5.5.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("com.atlassian.oai:swagger-request-validator-restassured:2.43.0")
-    testImplementation("io.rest-assured:kotlin-extensions:5.5.0")
+    // Test dependencies for Pact
+    testImplementation("com.atlassian.oai:swagger-request-validator-restassured:$swagger_request_validator_version")
+    testImplementation("io.rest-assured:kotlin-extensions:$rest_assured_version")
+    testImplementation("au.com.dius.pact.provider:junit5:$pact_version")
+    testImplementation("au.com.dius.pact.provider:spring6:$pact_version")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -37,4 +47,6 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+
+
 }
